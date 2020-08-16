3476913cbfc7d985cccdb01327a158c24b84fc45
Paul Walczak <polo.walczak@gmail.com>
2020-08-16 17:26:21 +0200

    🚧 Adding NavBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,27 @@
 </head>
 <body>
     <div id="app">
-
+        {{-- NavBar --}}
+        <nav class="w-screen flex shadow-lg bg-cultured-100 nav-bar">
+            <div class="flex justify-around w-full ">
+              <a><img class="my-2 " src="C:\Users\Polo\Documents\Perso\Projets\ShareYourProject\Logos\png\fit\Logo_viridiant_fit.png" width="100px" height="100px"></a>
+              <ul class="my-auto flex">
+                <li><a class="inline-flex">Services</a></li>
+                <li><a class="inline-flex">Project</a></li>
+                <li><a class="inline-flex">About</a></li>
+                <li><a class="inline-flex">Account</a></li>
+              </ul>
+              <div class="my-auto w-1/4">
+                <form role="search" class="w-full flex justify-center">
+                  <input class="search-bar" id="search-submit" type="text" placeholder="Search ...">
+                </form>
+              </div>
+              <div class="my-auto">
+                <button class="btn btn-viridiant-outline">Sign Up</button>
+                <button class="btn btn-viridiant">Log In</button>
+              </div>
+            </div>
+        </nav>
         <main>
             @yield('content')
         </main>
