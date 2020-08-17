700395ec0536b66ff2424516ea8e7beb328be9c1
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-17 21:35:56 +0200

    🗃 Update User Model

---
+++ database/migrations/2020_08_17_190152_update_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'username');
+            $table->string('first_name');
+            $table->string('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('username', 'name');
+            $table->dropColumn(['first_name', 'last_name']);
+        });
+    }
+}
