c86be0b396d45b05a9a1d4fd60c59ec657c63c39
Paul Walczak <polo.walczak@gmail.com>
2020-08-17 22:26:01 +0200

    🚧 Creating 2nd NavBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
     <div id="app">
         {{-- NavBar --}}
-        <nav class="w-full flex shadow-lg bg-cultured-100 nav-bar">
+        {{-- <nav class="w-full flex shadow-lg bg-cultured-100 nav-bar">
             <div class="flex justify-around w-full ">
               <a><img class="my-2 " src="{{asset('vendor/courier/logos/png/fit/Logo_viridiant_fit.png')}}" width="100px" height="100px"></a>
               <ul class="my-auto flex">
@@ -43,6 +43,36 @@
                 <button class="btn btn-viridiant-outline">Sign Up</button>
                 <button class="btn btn-viridiant">Log In</button>
               </div>
+            </div>
+        </nav>--}}
+        <nav class="w-full flex shadow-lg bg-cultured-100 nav-bar">
+            <div class="flex justify-around w-full ">
+              <a><img class="my-2 " src="{{asset('vendor/courier/logos/png/fit/Logo_viridiant_fit.png')}}" width="80px" height="80px"></a>
+              <ul class="my-auto hidden lg:flex">
+                <li><a class="inline-flex">Services</a></li>
+                <li><a class="inline-flex">Project</a></li>
+                <li><a class="inline-flex">About</a></li>
+                <li><a class="inline-flex">Account</a></li>
+              </ul>
+              <div class="my-auto w-1/2">
+                <form role="search" class="w-full justify-center">
+                  <div class="flex justify-center">
+                    <input class="search-box" id="search-submit" type="text" placeholder="Search ...">
+                    <button class="search-glass"><a><img src="{{asset('vendor/courier/icons/search.svg')}}" width="24px" height="24px"></a></button>
+                	</div>
+                </form>
+              </div>
+              <div class="my-auto hidden lg:block">
+                <button class="btn btn-viridiant-outline">Sign Up</button>
+                <button class="btn btn-viridiant">Log In</button>
+              </div>
+              <div class="my-auto flex lg:hidden">
+                <div class="my-auto menu-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            	</div>
             </div>
         </nav>
         <main>
