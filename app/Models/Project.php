ea831a679ed53f89828ce1f66788457fa273b627
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-20 22:07:43 +0200

    🗃 Adding Project Model and table

---
+++ app/Models/Project.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /**
+     * The name of the database.
+     *
+     * @var string
+     */
+    protected $table = 'projects';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'is_public'];
+
+    /**
+     * Get the user that owns the project.
+     */
+    public function author()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
