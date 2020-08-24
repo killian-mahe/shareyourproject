ebe7551495ae157fed9dbdaff6ec983f62ad238e
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-24 13:09:05 +0200

    🤡 Creating Post Factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'content' => $faker->text($maxNbChars = 255),
+    ];
+});
+
+$factory->state(Post::class, 'withAuthor', [
+    'author' => factory(App\User::class),
+]);
