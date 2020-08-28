0d7aa3b7af6a4a8779695e9cbd55dbe902233932
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-28 11:19:30 +0200

    ✨ Creating User routes

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('posts', 'PostController');
 
 Route::resource('projects', 'ProjectController');
+
+Route::resource('users', 'UserController')->except([
+    'create', 'store'
+]);
