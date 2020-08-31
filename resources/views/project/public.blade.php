7b802061f5f80ebd91a65c544dfa9d24bbc928a9
Paul Walczak <polo.walczak@gmail.com>
2020-09-01 00:02:35 +0200

    🚧 Public Page Header

--- resources/views/project/public.blade.php
+++ resources/views/project/public.blade.php
@@ -4,20 +4,20 @@
 {{-- <h1>{{$project->owner}}</h1> --}}
 
 <div class="w-full h-auto bg-cultured-400 flex justify-center py-6">
-    <div class="justify-center flex-row hidden sm:block w-1/4 md:w-1/4 lg:w-1/5">
+    <div class="sticky top-6 h-full justify-center flex-row hidden sm:block w-1/4 md:w-1/4 lg:w-1/6 bg-red-300">
         <div class="w-full h-120 bg-cultured-100 rounded-lg shadow-md"></div>
     </div>
-    <div class="flex-col w-full mx-2 sm:mx-5 sm:w-3/5 md:w-2/3 lg:w-1/2">
-        <div class="w-full h-auto mb-6 shadow flex-col">
+    <div class="w-full mx-2 sm:mx-5 sm:w-3/5 md:w-2/3 lg:w-1/2 rounded-lg">
+        <div class="w-full h-auto mb-6 shadow bg-cultured-100 md:rounded-lg">
             <div class="flex relative justify-center">
                 <img class=" md:rounded-lg object-cover w-full h-48 shadow-md" src="https://cdn.pixabay.com/photo/2013/03/06/23/09/color-91059_1280.jpg" alt="test">
                 <img class=" border-4 border-cultured-100 shadow-md object-cover w-40 h-40 rounded-full mx-auto sm:left-8 lg:left-16 -bottom-12 absolute" src="https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg" alt="test">
             </div>
-            <div class="bg-red-300 text-right font-semibold font-sans text-2xl mr-1/7 my-4">{{$project->name}}</div>
-            <div class="">{{$project->owner->username}}</div>
+            <div class="text-center md:text-right font-semibold font-sans text-2xl md:mr-1/7 mt-12 md:my-4">{{$project->name}}</div>
+            <span class="bg-yellow-300">{{$project->owner->username}}</span>
             <div class="">{{$project->created_at}}</div>
-            <div class="w-full h-auto bg-yellow-300">Tags</div>
-            <div class="w-full h-auto bg-indigo-300">badges</div>
+            <div class="tags w-full h-auto bg-yellow-300"></div>
+            <div class="badges w-full h-auto bg-indigo-300"></div>
         </div>
 
         @foreach ($project->posts as $post)
@@ -57,9 +57,6 @@
 
         @endforeach
 
-    </div>
-    <div class="justify-center flex-row hidden lg:w-1/5 lg:block">
-        <div class="w-full h-120 bg-cultured-100 rounded-lg shadow-md"></div>
     </div>
 </div>
 @endsection
