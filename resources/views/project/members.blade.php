a146b509e582fc3d45c648519f1258de4bdc71a7
Paul Walczak <polo.walczak@gmail.com>
2020-09-10 21:35:17 +0200

    💄 Adding Member-Card-Right

--- resources/views/project/members.blade.php
+++ resources/views/project/members.blade.php
@@ -2,18 +2,41 @@
 
 @section('main')
 
-    @foreach ($project->members as $member)
+    @php
+        $i = 0;
+    @endphp
 
-        <div class="member-card lg:flex md:rounded-lg p-6 shadow">
-            <a href="#"><img class="lg:h-32 lg:w-32 rounded-full mx-auto lg:ml-0 lg:mr-6 hover:shadow-md transform ease-in-out duration-200 hover:scale-105" src="https://images.unsplash.com/photo-1569007889977-42e2392850d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80"></a>
-            <div class="text-center lg:text-left">
-                <a href="#" class="member-card-name">{{$member->full_name()}}</a>
-                <div class="member-card-job">Photographer</div>
-                <a href="#" class="member-card-email">{{$member->email}}</a>
-            </div>
-        </div>
+        @foreach ($project->members as $member)
 
-    @endforeach
+            @if (!$i%2)
+
+                <div class="member-card lg:flex md:rounded-lg shadow">
+                    <a href="#"><img class="member-card-image lg:h-22 lg:w-22 mx-auto lg:ml-0 lg:mr-6" src="https://images.unsplash.com/photo-1569007889977-42e2392850d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80"></a>
+                    <div class="text-center lg:text-left">
+                        <a href="#" class="member-card-name">{{$member->full_name()}}</a>
+                        <div class="member-card-job">Photographer</div>
+                        <a href="#" class="member-card-email">{{$member->email}}</a>
+                    </div>
+                </div>
+
+            @else
+
+                <div class="member-card-right lg:flex md:rounded-lg shadow">
+                    <div class="text-center lg:text-right">
+                        <a href="#" class="member-card-name">{{$member->full_name()}}</a>
+                        <div class="member-card-job">Photographer</div>
+                        <a href="#" class="member-card-email">{{$member->email}}</a>
+                    </div>
+                    <a href="#"><img class="member-card-image lg:h-22 lg:w-22 mx-auto lg:mr-0 lg:ml-6" src="https://images.unsplash.com/photo-1569007889977-42e2392850d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80"></a>
+                </div>
+
+            @endif
+
+            @php
+                $i += 1;
+            @endphp
+
+        @endforeach
 
 
 @endsection
