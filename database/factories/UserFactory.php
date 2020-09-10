3769cf061761494cb576c8e4e2f320b90f29793a
Killian Mahé <killianmahe.pro@gmail.com>
2020-09-10 23:14:41 +0200

    🤡 Updating User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'last_name' => $faker->lastName,
         'username' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
+        'title' => $faker->jobTitle,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
