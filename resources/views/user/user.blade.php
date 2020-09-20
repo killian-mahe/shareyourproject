004f9bcdd5b43641f0736cb36322afffcead7987
Killian Mahé <killianmahe.pro@gmail.com>
2020-09-20 22:12:30 +0200

    ✨ Basic User public page

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -12,36 +12,19 @@
     <span class="font-light text-xs block text-right">Member since {{$user->created_at}}</span>
 </div>
 
-<h2 class="category-title my-2">Projects</h2>
+<h2 class="font-semibold text-xl my-5 ml-3">Projects</h2>
 
 @foreach ($user->projects as $project)
 
-<div class="bg-cultured-100 shadow md:rounded-lg flex mb-3 border-b-8 border-viridiant-600 w-3/4 min-h-14">
-    <img class="w-1/4 md:rounded-tl-md object-cover" src="{{asset('vendor/courier/img/jeff-sheldon-8z2Q6XWLYa4.jpg')}}" alt="project_picture">
-    <div class="p-3 flex flex-col justify-between">
-        <div>
-            <h3 class="font-semibold mb-4">{{$project->name}}</h3>
-            <p class="font-light text-sm">{{$project->description}}</p>
-            <div class="mt-4 mb-2">
-                @foreach ($project->technologies as $technology)
-                    <badge-label label="{{$technology->label}}" link="#"></badge-label>
-                @endforeach
-            </div>
-        </div>
-        <div class="w-full flex justify-end items-center">
-            <span class="relative w-24 h-7">
-                @foreach ($project->members->slice(0, 1) as $member)
-                    <img class="h-7 w-7 rounded-full absolute right-4" src="{{$member->profile_picture()}}" alt="">
-                @endforeach
+    @include('components.cards.project')
 
-                @foreach ($project->members->slice(1, 1) as $member)
-                    <img class="h-7 w-7 rounded-full absolute right-0" src="{{$member->profile_picture()}}" alt="">
-                @endforeach
-            </span>
-            <span class="text-sm font-light ml-2">{{$project->members->count()}} members</span>
-        </div>
-    </div>
-</div>
+@endforeach
+
+<h2 class="font-semibold text-xl my-5 ml-3">Posts</h2>
+
+@foreach ($user->posts as $post)
+
+    @include('components.post')
 
 @endforeach
 
