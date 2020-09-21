6b0e46b5a5ef96dc8a9398e0f9e7c15484180d46
Killian Mahé <killianmahe.pro@gmail.com>
2020-09-21 11:13:20 +0200

    ♻️ Refactoring box component

--- resources/views/project/layout.blade.php
+++ resources/views/project/layout.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
 
-<div class="w-full h-auto bg-cultured-400 flex justify-center py-6">
-    <div class="h-128 justify-center flex-row hidden sm:block w-1/4 md:w-1/4 lg:w-1/6">
-        <div class="w-full h-auto bg-cultured-100 rounded-lg shadow mb-4 flex flex-col flex-none py-2 mt-48">
+<div class="flex justify-center mt-4 min-h-screen">
+
+    <div class="hidden md:block md:w-1/3 md:ml-4 lg:w-1/4 xl:w-1/6">
+        <div class="box w-full h-auto mb-4 flex flex-col flex-none py-2 p-5 mt-48">
             <a class="py-2 px-6" href="{{route('projects.show', ['project'=>$project->id])}}">Publications</a>
             <a class="py-2 px-6" href="{{route('projects.members', ['project'=>$project->id])}}">Members</a>
             <a class="py-2 px-6" href="{{route('projects.about', ['project'=>$project->id])}}">About</a>
         </div>
     </div>
-    <div class="w-full mx-4 sm:mx-5 sm:w-3/5 md:w-2/3 lg:w-1/2 rounded-lg">
-        <div class="w-full h-auto mb-6 shadow bg-cultured-100 md:rounded-lg">
+
+    <div class="w-full md:w-2/3 lg:w-1/2 mx-4">
+
+        <div class="box w-full h-auto mb-6">
             <div class="flex relative justify-center">
-                <img class=" md:rounded-t-lg object-cover w-full h-48 shadow" src="https://cdn.pixabay.com/photo/2013/03/06/23/09/color-91059_1280.jpg" alt="test">
+                <img class=" md:rounded-t-md object-cover w-full h-48 shadow" src="https://cdn.pixabay.com/photo/2013/03/06/23/09/color-91059_1280.jpg" alt="test">
                 <img class=" border-4 border-cultured-100 shadow-md object-cover w-40 h-40 rounded-full mx-auto sm:left-8 lg:left-16 -bottom-12 absolute" src="https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg" alt="test">
             </div>
             <div class="text-center md:text-right font-semibold font-sans text-2xl md:mr-1/7 mt-12 md:my-4">{{$project->name}}</div>
@@ -34,6 +37,8 @@
         @yield('main')
 
     </div>
+    <div class="hidden xl:block lg:w-1/6 xl:w-1/8"></div>
+
 </div>
 
 @endsection
--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -5,7 +5,7 @@
 <div class="flex justify-center mt-4 min-h-screen">
 
     <div class="hidden md:block md:w-1/3 md:ml-4 lg:w-1/4 xl:w-1/6">
-        <div class="w-full h-auto bg-cultured-100 rounded-lg shadow mb-4 flex flex-col flex-none py-2 p-5 mt-48">
+        <div class="box w-full h-auto mb-4 flex flex-col flex-none py-2 p-5 mt-48">
             <h2 class="category-title">Links</h2>
             <div class="flex justify-between py-2"><span class="text-linkedin">LinkedIn</span><a href="https://www.linkedin.com/in/killian-mah%C3%A9-246928135/">killian-mahe</a></div>
             <div class="flex justify-between py-2"><span class="text-github">GitHub</span><a href="https://github.com/killian-mahe">@killian-mahe</a></div>
@@ -14,9 +14,9 @@
 
     <div class="w-full md:w-2/3 lg:w-1/2 mx-4">
 
-        <div class="w-full h-auto mb-3 pb-4 shadow bg-cultured-100 md:rounded-lg">
+        <div class="box w-full h-auto mb-3 pb-4">
             <div class="flex relative justify-center">
-                <img class="md:rounded-t-lg object-cover w-full h-48" src="https://cdn.pixabay.com/photo/2013/03/06/23/09/color-91059_1280.jpg" alt="test">
+                <img class="md:rounded-t-md object-cover w-full h-48" src="https://cdn.pixabay.com/photo/2013/03/06/23/09/color-91059_1280.jpg" alt="test">
                 <img class=" border-4 border-cultured-100 shadow-md object-cover w-40 h-40 rounded-full mx-auto sm:left-8 lg:left-16 -bottom-12 absolute" src="https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg" alt="test">
             </div>
             <div class="text-center md:text-right font-semibold font-sans text-2xl md:mr-1/7 mt-12 md:my-4">{{$user->first_name}} {{$user->last_name}}</div>
