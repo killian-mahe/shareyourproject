350f54d2c351b3cd602cd5a487c3100cabca4e36
Killian Mahé <killianmahe.pro@gmail.com>
2020-09-23 14:16:16 +0200

    🗃 Ceating intermediate post comment table

---
+++ database/migrations/2020_09_23_121453_create_comment_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_post', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('comment_id')->nullable(false);
+            $table->unsignedBigInteger('post_id')->nullable(false);
+
+            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_post');
+    }
+}
