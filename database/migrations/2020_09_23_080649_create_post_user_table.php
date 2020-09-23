b6fd478e63c0ccd0619772c9007db8d5bbedd2c6
Killian Mahé <killianmahe.pro@gmail.com>
2020-09-23 10:14:37 +0200

    🗃 Adding post_user table

---
+++ database/migrations/2020_09_23_080649_create_post_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->unsignedBigInteger('post_id')->nullable(false);
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_user');
+    }
+}
