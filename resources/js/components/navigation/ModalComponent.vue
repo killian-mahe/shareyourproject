2386a013d6338b2fa448c41fc1aa7619e76649a1
Killian <killianmahe.pro@gmail.com>
2020-11-11 18:25:29 +0100

    ✨ Default modal

---
+++ resources/js/components/navigation/ModalComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="modal-mask">
+        <div class="modal-wrapper">
+            <div class="modal-container">
+
+                <div class="modal-header">
+                <slot name="header">
+                    default header
+                </slot>
+                </div>
+
+                <div class="modal-body">
+                <slot name="body">
+                    default body
+                </slot>
+                </div>
+
+                <div class="modal-footer">
+                <slot name="footer">
+                    default footer
+                    <button class="float-right" @click="$emit('close')">
+                    OK
+                    </button>
+                </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    props: {
+    },
+    methods: {
+    }
+}
+</script>
