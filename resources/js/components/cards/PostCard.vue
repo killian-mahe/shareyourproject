8fe6677db320cd4e852c8d763cd053816936e667
Paul WALCZAK <polo.walczak@gmail.com>
2020-11-15 11:44:09 +0100

    💄 Adding Share Modal

--- resources/js/components/cards/PostCard.vue
+++ resources/js/components/cards/PostCard.vue
@@ -25,7 +25,9 @@
                 <span class="hover:text-orange-peel-400 cursor-pointer" @click="first_comment = true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg><span class="ml-1 hidden md:inline">Comment</span></span>
             </div>
             <div class="card-link">
-                <span class="hover:text-viridiant-400 cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg><span class="ml-1 hidden md:inline">Share</span></span>
+
+                <span class="hover:text-viridiant-400 cursor-pointer" @click="on_share=true"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg><span class="ml-1 hidden md:inline">Share</span></span>
+
             </div>
         </div>
         <div v-if="post.comments_overview.length > 0 || first_comment" class="p-5 mb-2 border-t-0.0625 border-onyx-100">
@@ -37,17 +39,36 @@
             <comment-component v-for="comment in post.comments_overview" :key="comment.id" :comment="comment" class="mb-3"></comment-component>
             <button v-if="comments_to_load.length > 0" class="btn-classic w-full font-sans text-sm" @click="addComments">Load more comments</button>
         </div>
+
+        <modal-component v-if='on_share'>
+            <template v-slot:header>
+                <div class="border-b pb-3 flex justify-between items-center">
+                    <h1 class="font-semibold text-onyx-600 text-xl">Share it !</h1>
+                    <svg class="feather feather-x cursor-pointer text-onyx-600" @click="on_share = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                </div>
+            </template>
+            <template v-slot:body>
+                <div class="my-4 flex w-full items-center">
+                    <svg @click='copy' class="feather feather-copy text-onyx-600 cursor-pointer mr-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+                    <input id="copy_link" disabled class="select-all w-full appearance-none block bg-gray-200 px-4 py-2 border border-gray-300 text-onyx-800 rounded-md" type="text" value="lllliiiinnnnkkk in description">
+                </div>
+            </template>
+            <template v-slot:footer><div></div></template>
+
+        </modal-component>
     </div>
 </template>
 
 <script>
     import CommentComponent from '../posts/CommentComponent.vue';
     import TextArea from '../inputs/TextArea.vue';
+    import ModalComponent from '../navigation/ModalComponent.vue';
 
     export default {
         components: {
             CommentComponent,
-            TextArea
+            TextArea,
+            ModalComponent,
         },
         data() {
             return {
@@ -55,7 +76,8 @@
                 comments: this.post_props.comments_overview,
                 comments_to_load: [],
                 first_comment : false,
-                newCommentContent: ""
+                newCommentContent: "",
+                on_share: false,
             }
         },
         props: {
@@ -66,7 +88,16 @@
         beforeMount() {
             this.comments_to_load = this.post.comments_ids.filter(comment_id => !this.comments.map(x => x.id).includes(comment_id));
         },
+        mounted() {
+            feather.replace();
+        },
         methods: {
+            copy: function() {
+                var copyText = document.querySelector("#copy_link");
+                console.log(copyText);
+                copyText.select();
+                document.execCommand("copy");
+            },
             like: function(like) {
                 if (like == true) {
                     axios.get('/api/posts/'+this.post.id+'/like')
