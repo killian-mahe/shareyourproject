f06d63a0ea229427a30098514f4188a5a992440d
Killian <killianmahe.pro@gmail.com>
2020-12-30 21:55:49 +0100

    ✨ Creating post modal Vue component

--- resources/js/components/posts/PostModal.vue
+++ resources/js/components/posts/PostModal.vue
@@ -5,49 +5,66 @@
                 <div></div>
             </template>
             <template v-slot:body>
-                <div class="h-160 flex flex-row">
+                <div class="h-160 flex flex-col lg:flex-row">
                     <!-- Carousel -->
-                    <div class="w-3/4 bg-gray-800"></div>
+                    <div class="w-full lg:w-3/4 h-120 lg:h-auto bg-gray-800 rounded-md relative">
+
+                        <!-- Left button -->
+                        <span class="top-1/2 left-0 absolute">
+                            <svg @click="prev()" xmlns="http://www.w3.org/2000/svg" class="feather feather-chevron-left carousel-button" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                        </span>
+
+                        <img :src="post.images_url[index]" class="carousel-image" alt="currentImage">
+
+                        <!-- Right button -->
+                        <span class="top-1/2 absolute right-0">
+                            <svg @click="next()" xmlns="http://www.w3.org/2000/svg" class="feather feather-chevron-right carousel-button" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                        </span>
+
+                        <!-- Indicator -->
+                        <span class="rounded-full bg-onyx-500 text-sm px-2 py-1 absolute bottom-2 right-2 text-cultured-700">
+                            {{index+1}}/{{post.images_url.length}}
+                        </span>
+
+                    </div>
 
                     <!-- Sidebar -->
-                    <div class="w-1/4">
+                    <div class="w-full lg:w-1/4">
                         <!-- Post -->
-                        <div class="w-full px-4 bg-white">
-                            <div v-if="post.project" class="flex">
-                                <a :href="post.project.url.index" class="w-auto inline-grid">
-                                    <img class="rounded-full my-auto h-10 w-10 object-cover hover:shadow-md" :src="post.project.profile_picture"/>
-                                </a>
-                                <div class="flex flex-col ml-3 justify-start">
-                                    <div class="my-auto text-lg"><a class="font-bold" :href="post.project.url.index">{{ post.project.name}}</a></div>
+                        <div class="w-full mt-4 lg:mt-0 lg:pl-4 bg-white">
+                            <div class="flex justify-between items-center">
+
+                                <!-- If post from a project -->
+                                <div v-if="post.project" class="flex">
+                                    <a :href="post.project.url.index" class="w-auto inline-grid">
+                                        <img class="rounded-full my-auto h-10 w-10 object-cover hover:shadow-md" :src="post.project.profile_picture"/>
+                                    </a>
+                                    <div class="flex flex-col ml-3 justify-start">
+                                        <div class="my-auto text-lg"><a class="font-bold" :href="post.project.url.index">{{ post.project.name}}</a></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div v-else class="flex">
-                                <a :href="post.url.author" class="w-auto inline-grid">
-                                    <img class="rounded-full my-auto h-10 w-10 object-cover hover:shadow-md" :src="post.author.profile_picture"/>
-                                </a>
-                                <div class="flex flex-col ml-3 justify-start">
-                                    <div><a class="font-bold" :href="post.url.author">{{ post.author.first_name }} {{post.author.last_name}}</a></div>
-                                    <span class="font-sans italic text-base">{{ post.author.title }}</span>
+                                <!-- Else -->
+                                <div v-else class="flex">
+                                    <a :href="post.url.author" class="w-auto inline-grid">
+                                        <img class="rounded-full my-auto h-10 w-10 object-cover hover:shadow-md" :src="post.author.profile_picture"/>
+                                    </a>
+                                    <div class="flex flex-col ml-3 justify-start">
+                                        <div><a class="font-bold" :href="post.url.author">{{ post.author.first_name }} {{post.author.last_name}}</a></div>
+                                        <span class="font-sans italic text-base">{{ post.author.title }}</span>
+                                    </div>
                                 </div>
+
+                                <!-- Like button -->
+                                <span v-if="post.liked" class="text-red-600 fill-current">
+                                    <svg @click="$emit('unlike')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-heart h-5 w-auto inline-block cursor-pointer" fill="true" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                </span>
+                                <span class="hover:text-red-600 fill-current" v-else>
+                                    <svg @click="$emit('like')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-heart h-5 w-auto inline-block cursor-pointer" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                </span>
                             </div>
 
-                            <div class="py-2">{{post.content}}</div>
-
-                            <div class="cursor-pointer hover:text-red-600 fill-current flex justify-center items-center w-full h-8 border-t-1 border-b-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-heart h-5 w-auto inline-block" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                                <span class="ml-1">I like</span>
-                            </div>
-                        </div>
-
-
-                        <!-- Comments -->
-                        <div class="p-2 mb-2 border-onyx-100">
-                            <div v-if="auth_user != null" class="mb-3 flex relative items-start">
-                                <text-area class="w-full" :rows="1" child_class="w-full pr-10 overflow-y-hidden resize-none" placeholder="Write a comment..." v-model="newCommentContent"></text-area>
-                                <svg class="feather feather-send absolute right-4 top-3 cursor-pointer" @click="writeComment" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
-                            </div>
-                            <comment-component v-for="comment in post.comments_overview" :key="comment.id" :comment="comment" class="mb-3"></comment-component>
-                            <!-- <button v-if="comments_to_load.length > 0" class="btn-classic w-full font-sans text-sm" @click="addComments">Load more comments</button> -->
+                            <!-- Post text content -->
+                            <div class="py-2 border-b-1 text-sm">{{post.content}}</div>
                         </div>
                     </div>
                 </div>
@@ -76,9 +93,31 @@ export default {
             required: true
         }
     },
-    mounted() {
-
+    data() {
+        return {
+            index: 0
+        }
+    },
+    methods: {
+        next() {
+            this.index++;
+            if (this.index >= this.post.images_url.length) this.index = 0;
+        },
+        prev() {
+            this.index--;
+            if (this.index < 0) this.index = this.post.images_url.length - 1;
+        }
     }
 }
 </script>
+
+<style lang="scss">
+.carousel-button {
+    @apply w-10 h-auto text-cultured-800 hover:text-cultured-400 cursor-pointer select-none;
+}
+
+.carousel-image {
+    @apply max-h-full max-w-full object-contain object-center m-auto h-full;
+}
+</style>
 
