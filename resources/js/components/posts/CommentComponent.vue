9ce6bf387bfc6d4a6f5123fefce809df1eeedd29
Killian <killianmahe.pro@gmail.com>
2020-12-30 21:55:31 +0100

    💄 Resizing comment text

--- resources/js/components/posts/CommentComponent.vue
+++ resources/js/components/posts/CommentComponent.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="flex w-full">
         <img :src="comment.author.profile_picture" class="w-10 h-10 rounded-full" alt="">
-        <div class="rounded-b-xl rounded-tr-xl ml-5 bg-gray-200 w-full px-3 py-2">
+        <div class="rounded-b-xl rounded-tr-xl ml-3 bg-gray-200 w-full px-3 py-2">
             <div class="mb-1">
                 <span class="leading-4 text-sm font-medium block">{{comment.author.first_name}} {{comment.author.last_name}}</span>
-                <span class="leading-3 text-xs block italic">{{comment.author.title}}</span>
             </div>
-            <v-clamp autoresize :max-lines="3">
-                <template slot="default">
-                    {{ comment.content }}
-                </template>
-                <template slot="after" slot-scope="expand">
-                    <span v-if="expand.clamped" class="cursor-pointer ml-2 text-base leading-6 font-semibold text-onyx-500" @click="expand.expand()">more</span>
-                </template>
-            </v-clamp>
+            <span class="text-sm">
+                <v-clamp autoresize :max-lines="3">
+                    <template slot="default">
+                        {{ comment.content }}
+                    </template>
+                    <template slot="after" slot-scope="expand">
+                        <span v-if="expand.clamped" class="cursor-pointer ml-2 text-base leading-6 font-semibold text-onyx-500" @click="expand.expand()">more</span>
+                    </template>
+                </v-clamp>
+            </span>
         </div>
     </div>
 </template>
@@ -33,7 +34,6 @@ import VClamp from 'vue-clamp';
             comment: Object
         },
         mounted() {
-            console.log(this.comment)
         }
     }
 </script>
