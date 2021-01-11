367220c2bf08571d40aa33f3fc1644693e8a51f9
Killian <killianmahe.pro@gmail.com>
2021-01-12 00:49:41 +0100

    ✨ Using new animated loading spinner

--- resources/js/components/lists/FeedView.vue
+++ resources/js/components/lists/FeedView.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <post-card v-for="post in posts" :key="post.id" :post_props="post" :auth_user="auth_user"></post-card>
+        <div class="h-10 my-3" ref="loader"></div>
     </div>
 </template>
 
 <script>
     import PostCard from '../cards/PostCard.vue';
     import {API} from '../../api';
+    import lottie from 'lottie-web';
 
     export default {
         components : {
@@ -14,7 +16,8 @@
         },
         data() {
             return {
-                posts: this.initial_posts
+                posts: this.initial_posts,
+                animation: null
             }
         },
         props: {
@@ -33,17 +36,34 @@
         },
         mounted() {
             this.scroll();
+            this.animation = lottie.loadAnimation({
+                container: this.$refs['loader'],
+                renderer: 'svg',
+                loop: true,
+                autoplay: false,
+                path: './vendor/courier/lottie/loading.json'
+            });
         },
         methods: {
+            startLoading() {
+                this.animation.play();
+            },
+            stopLoading() {
+                this.animation.stop();
+            },
             scroll () {
                 window.onscroll = () => {
                 let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
 
                 if (bottomOfWindow) {
+                    this.startLoading();
+
                     API.Post.feed(this.posts.map(post => post.id)).then(posts => {
                         posts.forEach(post => {
                             this.posts.push(post);
                         })
+
+                        this.stopLoading();
                     });
                 }
             };
