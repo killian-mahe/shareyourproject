1e0d5e0a3486d129c4cb039cf864615de82c4743
Killian Mahé <killianmahe.pro@gmail.com>
2021-02-03 00:38:20 +0100

    ✨ Creating PostCreated event

---
+++ app/Events/PostCreated.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use App\Models\Post;
+use Illuminate\Queue\SerializesModels;
+
+class PostCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The post instance
+     *
+     * @var \App\Models\Post
+     */
+    public $post;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \App\Models\Post  $post
+     * @return void
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        if ($this->post->project != NULL)
+        {
+            return new PrivateChannel('projects.'.$this->post->project->id);
+        }
+
+        return [];
+    }
+}
