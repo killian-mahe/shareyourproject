5d037d0ca5c4c04911543ce17c539435cfd094e4
Killian Mahé <killianmahe.pro@gmail.com>
2021-03-14 23:47:01 +0100

    ✨ Links in settings

--- resources/views/user/edit/layout.blade.php
+++ resources/views/user/edit/layout.blade.php
@@ -6,9 +6,9 @@
 
     <div class="block w-auto md:mx-4 lg:w-1/4 xl:w-1/6">
         <div class="box w-full h-auto mb-4 flex lg:flex-col flex-none py-2 p-5 md:mt-4 justify-center lg:mt-48 md:space-x-8 lg:space-x-0">
-            <a class="py-2 px-6" href="#">Profile</a>
-            <a class="py-2 px-6" href="#">Account</a>
-            <a class="py-2 px-6" href="#">Notifications</a>
+            <a class="py-2 px-6 {{Route::is('users.settings.profile') ? 'active' : ''}}" href="{{route('users.settings.profile', ['user' => $user])}}">Profile</a>
+            <a class="py-2 px-6 {{Route::is('users.settings.account') ? 'active' : ''}}" href="{{route('users.settings.account', ['user' => $user])}}">Account</a>
+            <a class="py-2 px-6 {{Route::is('users.settings.notif') ? 'active' : ''}}" href="{{route('users.settings.notif', ['user' => $user])}}">Notifications</a>
         </div>
     </div>
 
