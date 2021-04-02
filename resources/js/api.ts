61afcb6557531f6179a09e51f2f989915aa9e975
Killian Mahé <killianmahe.pro@gmail.com>
2021-04-02 14:08:04 +0200

    👽 Update Like Vue API

--- resources/js/api.ts
+++ resources/js/api.ts
@@ -80,177 +80,18 @@ let API = {
          * Like a post
          * @param id
          */
-        like: function(id: number) {
-            const url = `${this.url}/${id}/like`;
-            return fetchResource('get', url);
+        like: function(post: Post) {
+            const url = `${this.url}/${post.id}/like`;
+            return fetchResource('put', url);
         },
         /**
          * Unike a post
          * @param id
          */
-        unlike: function(id: number) {
-            const url = `${this.url}/${id}/unlike`;
-            return fetchResource('get', url);
+        unlike: function(post: Post) {
+            const url = `${this.url}/${post.id}/unlike`;
+            return fetchResource('put', url);
         },
-        /**
-         * Create a new post
-         * @param content Post content
-         * @param project_author Project author id
-         * @param reshare Reshared post id
-         * @param images Post images
-         */
-        create: function(content: string, project_author : number | null = null, reshare : number | null = null, images: Array<File> | null = null) {
-            const url = `${this.url}`;
-            let formData = new FormData();
-            // Post content
-            formData.append('content', content);
-
-            // Project author (if necessary)
-            if (project_author) formData.append('project', String(project_author));
-
-            // Post reshared (if necessary)
-            if (reshare) formData.append('reshare', String(reshare));
-
-            // Post images
-            if (images) {
-                images.forEach((image, index) => {
-                    formData.append(`image[${index}]`, image);
-                });
-            }
-            return fetchResource<Post>('post', url, formData, {
-                'Content-Type': 'multipart/form-data'
-            });
-        }
-    },
-    /**
-     * User API wrapper
-     */
-    User: {
-        /**
-         * Base user request url
-         */
-        url: '/users',
-        /**
-         * Search users that correspond to the given query string
-         * @param query Query string
-         */
-        search: function(query: string) {
-            const url = `${this.url}/search/${query}`;
-            return fetchResource<Array<User>>('get', url);
-        },
-        /**
-         * Get the corresponding user
-         * @param id User id
-         */
-        get: function(id: string) {
-            const url = `${this.url}/${id}`;
-            return fetchResource<User>('get', url);
-        }
-    },
-    /**
-     * Project API wrapper
-     */
-    Project: {
-        /**
-         * Base project requests url
-         */
-        url: '/projects',
-        /**
-         * Get a project corresponding to the id passed to the function
-         * @param id Project id
-         */
-        get: function(id: number) {
-            const url = `${this.url}/${id}`;
-            return fetchResource<Project>('get', url);
-        },
-        /**
-         * Make the authenticated user follows the project
-         * @param id Project id
-         */
-        follow: function(id: number) {
-            const url = `${this.url}/${id}/follow`;
-            return fetchResource('get', url);
-        },
-        /**
-         * Make the authenticated user unfollows the project
-         * @param id Project id
-         */
-        unfollow: function(id :number) {
-            const url = `${this.url}/${id}/unfollow`;
-            return fetchResource('get', url);
-        },
-        /**
-         * Get projects corresponding to the ids passed to the function
-         * @param projects_ids Project ids to restrieve
-         */
-        getMany: function(projects_ids: Array<number> = []) {
-            const url = `${this.url}/get`;
-            return fetchResource<Array<Project>>('post', url, {projects_ids: projects_ids});
-        }
-    },
-    /**
-     * Tag API wrapper
-     */
-    Tag: {
-        /**
-         * Base tag requests url
-         */
-        url: '/tags',
-        /**
-         * Search tags that correspond to the given query string
-         * @param {string} query Query string
-         * @return {Promise<Array<Tag>>} Tags
-         */
-        // search: function(query: string) : Promise<Array<Tag>> {
-        //     const url = `${this.url}/search/${query}`;
-        //     return fetchResource('get', url);
-        // }
-    },
-    /**
-     * Badge API wrapper
-     */
-    Badge: {
-        /**
-         * Base badge requests url
-         */
-        url: '/badges',
-        /**
-         * Search badges that correspond to the given query string
-         * @param query Query string
-         */
-        search: function(query: string) {
-            const url = `${this.url}/search/${query}`;
-            return fetchResource<Array<Badge>>('get', url);
-        }
-    },
-    /**
-     * Comment API wrapper
-     */
-    Comment: {
-        /**
-         * Base comment requests url
-         */
-        url: '/comments',
-        /**
-         * Get many comments
-         * @param ids
-         */
-        getMany : function(ids: Array<number>) {
-            const url = `${this.url}/get`;
-            return fetchResource<Array<Comment>>('post', url, {comments_ids: ids});
-        },
-        /**
-         * Create a new comment
-         * @param content Comment content
-         * @param post Post id
-         */
-        create: function(content: string, post: number) {
-            const url = this.url;
-            return fetchResource<Comment>('post', url, {
-                content: content,
-                post_id: post
-            });
-        }
     },
     Feed: {
         /**
