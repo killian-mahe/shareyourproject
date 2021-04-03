160ed8af4ae3137a924b95b384f5e20c45ee094e
Killian Mahé <killianmahe.pro@gmail.com>
2021-04-03 17:15:08 +0200

    🐛 Remove depreciated middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,9 +17,7 @@ class UserController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->except([
-            'search'
-        ]);
+        
     }
 
 
